89b406f19a1237e9b0a510b195fca279f8eeac1c
dhruser <dheerajshetty2002@gmail.com>
2024-06-30 21:07:18 +0530

    first commit

---
+++ 1.java
@@ -0,0 +1,5 @@
+class 1{
+    public static void main(Strings[] args){
+        System.out.println("Print the output");     
+    }
+}
---
+++ 2.java
@@ -0,0 +1,5 @@
+class 2{
+    public static void main(Strings[] args){
+        System.out.println("this is the capstone");     
+    }
+}
